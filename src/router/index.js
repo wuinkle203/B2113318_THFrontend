43501aba886b62a974954f2613dbb8852f42ae03
wuinkle203 <leq484437@gmai.com>
2024-03-03 16:46:42 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,17 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
